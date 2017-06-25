3ee302413819e465191989991ffc2e3d5739a1b9
David Waite <dwaite498@gmail.com>
2017-06-25 01:47:23 +0000

    bookmark new actions and views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,8 +4,24 @@ class BookmarksController < ApplicationController
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new
+  end
+  
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = @topic.bookmarks.new(bookmark_params)
+    # @bookmark.user = current_user
+    @bookmark.save
+    redirect_to topic_path(@topic)
   end
 
   def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+  
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:url)
   end
 end
