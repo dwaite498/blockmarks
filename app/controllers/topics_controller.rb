c7e087429c3c2d6bc4ea82e48221322ad563d063
David Waite <dwaite498@gmail.com>
2017-07-02 22:57:48 +0000

    fix pundit implementation

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -28,6 +28,7 @@ class TopicsController < ApplicationController
   
   def destroy
     @topic = Topic.find(params[:id])
+    authorize @topic
     @topic.destroy
   end
 end
---
+++ app/policies/topic_policy.rb
@@ -0,0 +1,5 @@
+class TopicPolicy < ApplicationPolicy
+    
+
+    
+end
