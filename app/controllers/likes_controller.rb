b4b75b6a1322feb05cc5d9b62a05357c703412dc
David Waite <dwaite498@gmail.com>
2017-07-11 16:39:59 +0000

    WIP likes, bookmark view has error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,30 @@
 class LikesController < ApplicationController
   def index
   end
+  
+  def create
+     @bookmark = Bookmark.find(params[:bookmark_id])
+     like = current_user.likes.build(bookmark: @bookmark)
+ 
+     if like.save
+       flash[:success] = "Bookmark liked!"
+     else
+       flash[:alert] = "Unable to like bookmark, please try again."
+       
+       redirect_to [@bookmark.topic, @bookmark]
+     end
+   end
+
+   def destroy
+     @bookmark = Bookmark.find(params[:id])
+     @like = current_user.likes.find(params[:id])
+ 
+     if like.destroy
+       flash[:success] = "Bookmark liked!"
+     else
+       flash[:alert] = "Error, like not deleted!"
+       
+       redirect_to [@bookmark.topic, @bookmark]
+     end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   resources :users
   
   resources :topics do
-    resources :bookmarks
+    resources :bookmarks do
+      resources :likes, only: [:index, :create, :destroy]
+    end
   end
 
   root 'welcome#index'
